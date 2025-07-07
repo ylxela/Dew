5c875e69d6934cf25a69d6c17c3cc75d01fdec3c
UrWeird <p13sh03jr@gmail.com>
2025-07-07 19:10:15 +1000

    feat: idle and drag animations (bound to i and d)

--- Sprite.py
+++ Sprite.py
@@ -8,32 +8,60 @@ class Sprite:
         root.wm_attributes('-transparentcolor', 'white') # white pixels = transparent
         self.root.wm_attributes("-topmost", True)  # keep on top
         self.root.config()
-
-        # Load the GIF
-        self.frames = []
-        gif = Image.open("img.gif")
-        try:
-            while True:
-                frame = gif.copy().resize((160, 160), Image.Resampling.LANCZOS)  # todo: resize to what we need
-                frame = ImageTk.PhotoImage(frame)
-                self.frames.append(frame)
-                gif.seek(len(self.frames))
-        except EOFError:
-            pass
-
+        self.animations = {
+            "idle": self.load_img("idle.gif"),
+            "drag": self.load_img("drag.gif"),
+        }
+        self.curr_state = "idle"
 
         self.label = tk.Label(root)
         self.label.pack()
 
-        self.current_frame = 0
+        self.curr_frame = 0
         self.update_frame()
 
+    # Load the GIF
+    def load_img(self, path):
+        frames = []
+        gif = Image.open(path)
+        try:
+            while True:
+                frame = gif.copy().resize((160, 160), Image.Resampling.LANCZOS)
+                frames.append(ImageTk.PhotoImage(frame))
+                gif.seek(len(frames))
+        except EOFError:
+            pass
+        return frames
+
+
     def update_frame(self):
-        frame = self.frames[self.current_frame]
+        frames = self.animations[self.curr_state]
+        frame = frames[self.curr_frame]
         self.label.config(image=frame)
-        self.current_frame = (self.current_frame + 1) % len(self.frames)
-        self.root.after(100, self.update_frame)  # Adjust timing as needed
+        self.curr_frame = (self.curr_frame + 1) % len(frames)
+        self.root.after(100, self.update_frame)
+
+    
+    def toggle_visibility(self, event):
+        if root.state() == 'normal':
+            root.withdraw()
+        else:
+            root.deiconify()
+    
+    def drag(self, event):
+        self.curr_state = "drag"
+        self.curr_frame = 0
+
+    def idle(self, event):
+        self.curr_state = "idle"
+        self.curr_frame = 0
+
+
 
 root = tk.Tk()
+root.focus_force()
 app = Sprite(root)
+root.bind('<space>', app.toggle_visibility)
+root.bind('i', app.idle)
+root.bind('d', app.drag)
 root.mainloop()
